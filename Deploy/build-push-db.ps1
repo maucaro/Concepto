08f3662e5e79397c7f1c55d86679ba0aae2108c0
Mauricio Caro <maucaro@outlook.com>
2021-09-07 23:59:38 +0000

    Updated deployment to include Db

--- Deploy/build-push-db.ps1
+++ Deploy/build-push-db.ps1
@@ -1,5 +1,4 @@
 docker build ..\DbUsers\. -t dbsql:dev
 docker tag dbsql:dev us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
 docker push us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
-kubectl delete -f db.yaml 
-kubectl apply -f db.yaml
+
