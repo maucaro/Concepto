e88c7dd61b401eb456a41da8458c6872eac60e7d
Mauricio Caro <maucaro@outlook.com>
2021-09-09 20:09:22 +0000

    Fixed bug in replace.sh (would only substitute if 'image' not preceeded by '- ')

--- DbUsers/Data.sql
+++ DbUsers/Data.sql
@@ -4,6 +4,8 @@ INSERT [dbo].[Permissions] ([Id], [Name]) VALUES (N'013be2f2-9897-453c-b554-9c0d
 GO
 INSERT [dbo].[Tenants] ([Id], [Name]) VALUES (N'vida-75frq', N'Tenant de Vida18')
 GO
+INSERT [dbo].[Tenants] ([Id], [Name]) VALUES (N'caro-66frq', N'Tenant de Caro')
+GO
 INSERT [dbo].[Roles] ([Id], [Name], [TenantId]) VALUES (N'ed6a2371-b9b8-4288-8e54-1bbd62c56ba2', N'Operator', N'vida-75frq')
 GO
 INSERT [dbo].[Roles] ([Id], [Name], [TenantId]) VALUES (N'c68d551f-dbaf-4dd4-8f87-2d022e0b5233', N'Admin', N'vida-75frq')
--- Deploy/replace.sh
+++ Deploy/replace.sh
@@ -1,5 +1,5 @@
 REPLACE=$(printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g')
-sed -i "0,/^\([[:space:]]*image: *\).*/s//\1$REPLACE/;" $2
+sed -i "0,/^\([[:space:]]*[[:space:]\-] image: *\).*/s//\1$REPLACE/;" $2
 echo "Contents of " $2 
 echo "-------"
 cat $2
