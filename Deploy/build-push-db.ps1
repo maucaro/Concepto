0898522123746486446b3274151cd0d76f648cd3
Mauricio Caro <maucaro@outlook.com>
2021-09-09 23:17:36 +0000

    Fixed lack of tagging in build-push-db.sql

--- Deploy/build-push-db.ps1
+++ Deploy/build-push-db.ps1
@@ -1,4 +1,4 @@
-type ..\DbUsers\Dockerfile | docker build -f - ..
+type ..\DbUsers\Dockerfile | docker build -f - .. -t dbsql:dev
 docker tag dbsql:dev us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
 docker push us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
 
