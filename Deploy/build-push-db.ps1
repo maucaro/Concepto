8b56e87c1852b939d867ad227b666bd5e0ffffd7
Mauricio Caro <maucaro@outlook.com>
2021-09-09 15:40:12 +0000

    Fixing Db Azure DevOps build

--- Deploy/build-push-db.ps1
+++ Deploy/build-push-db.ps1
@@ -1,4 +1,4 @@
-docker build ..\DbUsers\. -t dbsql:dev
+type ..\DbUsers\Dockerfile | docker build -f - ..
 docker tag dbsql:dev us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
 docker push us-west2-docker.pkg.dev/utility-descent-185119/webapp-repo/dbsql:dev
 
