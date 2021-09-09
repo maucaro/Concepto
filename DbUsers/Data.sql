9228048a0e27fdf65689bf1ed61bfb032387c087
Mauricio Caro <maucaro@outlook.com>
2021-09-09 19:17:34 +0000

    Added _File subsitution to cloudbiuld.yaml.

--- DbUsers/Data.sql
+++ DbUsers/Data.sql
@@ -10,3 +10,6 @@ INSERT [dbo].[Roles] ([Id], [Name], [TenantId]) VALUES (N'c68d551f-dbaf-4dd4-8f8
 GO
 INSERT [dbo].[RolePermissions] ([RoleId], [PermissionId]) VALUES (N'c68d551f-dbaf-4dd4-8f87-2d022e0b5233', N'013be2f2-9897-453c-b554-9c0d8d3f0115')
 GO
+INSERT [dbo].[RolePermissions] ([RoleId], [PermissionId]) VALUES (N'ed6a2371-b9b8-4288-8e54-1bbd62c56ba2', N'013be2f2-9897-453c-b554-9c0d8d3f0115')
+GO
+
